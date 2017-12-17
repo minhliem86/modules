b53b2c6cb4f18923b638a2d5cfbb6cb1a337daa7
Liem Phan <minhliemphp@gmail.com>
2017-12-17 21:22:52 +0700

    first init

---
+++ ModulesServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+namespace Liemphan\modules;
+use Illuminate\Support\ServiceProvider;
+class ModulesServiceProvider extends ServiceProvider{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([__DIR__.'/config/module.php' => config_path('module.php')], 'module_config');
+        $this->publishes([__DIR__.'/Modules' => base_path('app/Modules')], 'module');
+        $this->publishes([__DIR__.'/Repositories' => base_path('app/Repositories')], 'module_repo');
+        $this->publishes([__DIR__.'/resources/assets' => public_path('/assets')], 'module_assets');
+        $this->publishes([__DIR__.'/resources/myLib' => base_path('/resources')], 'module_library');
+        $this->publishes([__DIR__.'/migrations' => base_path('database/migrations')], 'module_migration' );
+        $this->publishes([__DIR__ . '/Models' => base_path('app/Models')], 'module_model');
+        $this->publishes([__DIR__ . '/Providers/ComposerServiceProvider.php' => base_path('app/Providers')]);
+        $this->publishes([__DIR__ . '/ViewComposers' => base_path('app/ViewComposers')]);
+    }
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/Modules/Admin/Views/layouts/navigation.blade.php
@@ -0,0 +1,8 @@
+<div class="col-xs-6 col-sm-3 sidebar-offcanvas" role="navigation">
+  <ul class="list-group panel">
+    <li class="list-group-item"><b>SIDE PANEL</b></li>
+    <li class="list-group-item {{LP_lib::setActive(2,'dashboard')}}"><a href="{{route('admin.dashboard')}}"><i class="fa fa-dashboard"></i>Dashboard </a></li>
+
+
+  </ul>
+</div>
