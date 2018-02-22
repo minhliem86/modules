10035675a018299bd3a1cbaf15f690158c9e9311
Liem Phan <minhliemphp@gmail.com>
2018-02-22 23:39:54 +0700

    up command

--- src/ModulesServiceProvider.php
+++ src/ModulesServiceProvider.php
@@ -22,9 +22,9 @@ class ModulesServiceProvider extends ServiceProvider{
         $this->publishes([__DIR__.'/resources/myLib' => base_path('/resources/myLib')], 'module_library');
         $this->publishes([__DIR__.'/migrations' => base_path('database/migrations')], 'module_migration' );
         $this->publishes([__DIR__.'/Models' => base_path('app/Models')], 'module_model');
-        $this->publishes([__DIR__.'/Providers/ComposerServiceProvider.php' => base_path('app/Providers')]);
+        $this->publishes([__DIR__.'/Providers/ComposerServiceProvider.php' => base_path('app/Providers/ComposerServiceProvider.php')]);
         $this->publishes([__DIR__.'/ViewComposers' => base_path('app/ViewComposers')]);
-        $this->publishes([__DIR__.'/Kernel.php' => base_path('app/Http/Kernel.php')]);
+        $this->publishes([__DIR__.'/Kernel.php' => base_path('app/Http/Kernel.php')] );
     }
     /**
      * Register the application services.
